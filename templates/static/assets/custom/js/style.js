f8b504b4924418e96d201c505a8fc96cc28bda4f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-12-04 12:36:14 -0300

    Add base.html and partials templates.

---
+++ templates/static/assets/custom/js/style.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  t = 0;
+  $("#send").click(function (e) {
+    e.preventDefault();
+    var prompt = $("#prompt").val().trimEnd();
+    console.log(prompt);
+    if (prompt == "") {
+      $("#response").text("Please ask a question.");
+    } else {
+      function myTimer() {
+        $("#response").html("<p>Waiting for response ... " + t + "s</p>");
+        t++;
+      }
+      const myInterval = setInterval(myTimer, 1000);
+
+      var id = window.location.pathname.split('/').pop();
+            
+      $.ajax({
+        url: "/query/" + id,
+        method: "POST",
+        data: JSON.stringify({ input: prompt }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+          $("#response").html("<p>" + data.response + "</p>");
+          $("#response").append(
+            "<small class='text-secondary'>Responded in " +
+              t +
+              " seconds</small>"
+          );
+          $("#source").html(
+            "<small class='text-secondary'>" + data.source + "</small>"
+          );
+          clearInterval(myInterval);
+          t = 0;
+        },
+      });
+    }
+  });
+});
